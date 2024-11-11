d9effb5418e3a3db956d56aaf637e1b2d24f9fcb
MayurSaykhede <mayur3nda@gmail.com>
2024-11-11 13:34:39 +0530

    1st Commit

---
+++ src/test/java/sample/sampleClass1.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sampleClass1 {
+	
+	@Test
+	public void sample1() {
+		System.out.println("sample1()");
+	}
+
+}
---
+++ src/test/java/sample/sampleClass2.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sampleClass2 {
+	
+	@Test
+	public void sample2() {
+		System.out.println("sample2()");
+	}
+	
+}
